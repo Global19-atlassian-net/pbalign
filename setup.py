099dbcf8a9a2cf027052b51a9e99fe790088e9c9
David Seifert <dseifert@pacificbiosciences.com>
2018-08-17 20:58:11 +0200

    Version bump to 0.3.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 
 setup(
     name='pbalign',
-    version='0.3.0',
+    version='0.3.1',
     author='Pacific Biosciences',
     author_email='devnet@pacificbiosciences.com',
     license='LICENSE.txt',
