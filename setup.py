bf2839f341428f38960dd356379d08e9452861ba
David Seifert <dseifert@pacificbiosciences.com>
2018-08-17 20:58:11 +0200

    Modernize setup.py
    * Include `LICENSES.txt` in sdist-generated tarball

--- setup.py
+++ setup.py
@@ -4,25 +4,28 @@ import os
 import sys
 
 setup(
-    name = 'pbalign',
+    name='pbalign',
     version='0.3.0',
     author='Pacific Biosciences',
     author_email='devnet@pacificbiosciences.com',
     license='LICENSE.txt',
-    package_dir={'pbalign':'pbalign'},
-    packages = find_packages(),
-    zip_safe = False,
+    packages=find_packages(),
+    zip_safe=False,
     install_requires=[
         'pbcore >= 0.8.5',
         'pbcommand >= 0.2.0',
         'pysam',
-        ],
-    test_requires=['pbtestdata'],
+    ],
+    test_requires=[
+        'nose',
+        'pbtestdata',
+    ],
+    test_suite='nose.collector',
     entry_points={'console_scripts': [
         'pbalign=pbalign.pbalignrunner:main',
         'maskAlignedReads.py = pbalign.tools.mask_aligned_reads:main',
         'loadChemistry.py = pbalign.tools.loadChemistry:main',
         'extractUnmappedSubreads.py = pbalign.tools.extractUnmappedSubreads:main',
         'createChemistryHeader.py = pbalign.tools.createChemistryHeader:main'
-        ]}
-    )
+    ]}
+)
